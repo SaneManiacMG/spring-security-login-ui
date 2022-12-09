b2c84b5ea102f7144239cb49cba61e08aaf44b53
SaneManiacMG <mmoroane@hotmail.com>
2022-12-09 09:26:58 +0200

    Imported Java project

---
+++ spring-auth-app/src/main/java/com/smworks/springauthapp/SpringAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.smworks.springauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthAppApplication.class, args);
+	}
+
+}
---
+++ spring-auth-app/src/test/java/com/smworks/springauthapp/SpringAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smworks.springauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
