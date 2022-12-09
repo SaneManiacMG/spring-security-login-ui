03a3bdae68aaef05da4ba2576dadfa5a09230a6c
SaneManiacMG <mmoroane@hotmail.com>
2022-12-09 10:02:54 +0200

    Added table creation script

--- spring-auth-app/src/main/resources/data.sql
+++ spring-auth-app/src/main/resources/data.sql
@@ -1,2 +1,8 @@
+CREATE TABLE users (
+   username VARCHAR(45) NOT NULL , password VARCHAR(45) NOT NULL ,
+   account_non_locked TINYINT NOT NULL DEFAULT 1 ,
+   PRIMARY KEY (username)
+);
+
 INSERT INTO users(username,password,account_non_locked)
 VALUES ('user','12345', true);
